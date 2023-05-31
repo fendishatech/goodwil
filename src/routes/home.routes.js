f462c1eb3b48be7dd0d1e707ff63cd2d16ea34a4
fendishatech <liyukibir@gmail.com>
2023-05-31 07:03:55 -0700

    home page skeleton

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -12,91 +12,91 @@ router.get("/", async (req, res) => {
   }
 });
 
-// GET /new
-router.get("/new", (req, res) => {
-  res.render("blogs/new", { blog: new BlogPost() });
-});
+// // GET /new
+// router.get("/new", (req, res) => {
+//   res.render("blogs/new", { blog: new BlogPost() });
+// });
 
-// GET /:id
-router.get("/:id", async (req, res) => {
-  try {
-    const blog = await BlogPost.findByPk(req.params.id);
-    if (!blog) {
-      res.status(404).send("Blog Post Not Found");
-    } else {
-      res.render("blogs/show", { blog });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // GET /:id
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const blog = await BlogPost.findByPk(req.params.id);
+//     if (!blog) {
+//       res.status(404).send("Blog Post Not Found");
+//     } else {
+//       res.render("blogs/show", { blog });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// POST /
-router.post("/", async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const blog = await BlogPost.create({ title, content });
+// // POST /
+// router.post("/", async (req, res) => {
+//   try {
+//     const { title, content } = req.body;
+//     const blog = await BlogPost.create({ title, content });
 
-    return res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+//     return res.redirect("/");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// GET /:id/edit
-router.get("/:id/edit", async (req, res) => {
-  try {
-    const blog = await BlogPost.findByPk(req.params.id);
-    if (!blog) {
-      res.status(404).send("Blog Post Not Found");
-    } else {
-      res.render("blogs/edit", { blog });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // GET /:id/edit
+// router.get("/:id/edit", async (req, res) => {
+//   try {
+//     const blog = await BlogPost.findByPk(req.params.id);
+//     if (!blog) {
+//       res.status(404).send("Blog Post Not Found");
+//     } else {
+//       res.render("blogs/edit", { blog });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// PUT /:id
-router.put("/:id", async (req, res) => {
-  let blog;
+// // PUT /:id
+// router.put("/:id", async (req, res) => {
+//   let blog;
 
-  try {
-    const blog = await BlogPost.findByPk(req.params.id);
-    const title = req.body.title;
-    const content = req.body.content;
+//   try {
+//     const blog = await BlogPost.findByPk(req.params.id);
+//     const title = req.body.title;
+//     const content = req.body.content;
 
-    if (!blog) {
-      res.status(404).send("Blog Post Not Found");
-    } else {
-      const updatedBlog = await blog.update({ title, content });
-      res.redirect(`/`);
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+//     if (!blog) {
+//       res.status(404).send("Blog Post Not Found");
+//     } else {
+//       const updatedBlog = await blog.update({ title, content });
+//       res.redirect(`/`);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-// DELETE /blog-posts/:id
-router.delete("/:id", async (req, res) => {
-  console.log("Id :", req.params.id);
-  try {
-    const blog = await BlogPost.findByPk(req.params.id);
-    console.log("Blog :", blog);
-    if (!blog) {
-      res.status(404).send("Blog Post Not Found");
-    } else {
-      await blog.destroy();
-      res.redirect("/");
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // DELETE /blog-posts/:id
+// router.delete("/:id", async (req, res) => {
+//   console.log("Id :", req.params.id);
+//   try {
+//     const blog = await BlogPost.findByPk(req.params.id);
+//     console.log("Blog :", blog);
+//     if (!blog) {
+//       res.status(404).send("Blog Post Not Found");
+//     } else {
+//       await blog.destroy();
+//       res.redirect("/");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
 module.exports = router;
